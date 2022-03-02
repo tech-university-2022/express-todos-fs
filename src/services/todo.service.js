f712391f84908e8fdded3bb90e4df169f029dfd1
Sara-Jain <Sara_Jain@mckinsey.com>
2022-03-02 11:50:26 +0530

    refactor: modify variable name

--- src/services/todo.service.js
+++ src/services/todo.service.js
@@ -3,16 +3,6 @@ const fs = require("fs");
 const filePath = "./resources/todos.txt";
 const { promisfyReadFile, promisfyAppendFile, promisfyWriteFile, promisfyUpdateFile } = require('../utils/todos.utils');
 
-const listTodos = async () => {
-    const todoList = await fetchTodos(path);
-    const todoObject = [];
-    todoList.forEach((data) => {
-        const [todoId, todoTask] = data;
-        todoObject.push({ todoId, todoTask });
-    })
-    return todoObject
-};
-
 
 const getTodoService = async () => {
     const todos = await promisfyReadFile(filePath);
@@ -41,38 +31,18 @@ const deleteTodoService = async (id) => {
     todos.splice(id - 1, 1);
     console.log(`from line 42 ${todos}`);
     const modifiedTodo = todos.map((element, index) => {
-        if (index >= (id-1)) {
+        if (index >= (id - 1)) {
             element = `${(index + 1).toString()}|${element.split('|')[1]}`;
         }
         return element;
     });
-    modifiedTodoString =modifiedTodo.reduce((prevContent, current) => {
-            prevContent += current;
-            return prevContent;
-          }, '');
+    modifiedTodoString = modifiedTodo.reduce((prevContent, current) => {
+        prevContent += `${current}\n`;
+        return prevContent;
+    }, '');
     console.log(`from line 53 ${modifiedTodoString}`);
-    await promisfyWriteFile(filePath,modifiedTodoString);
-    //await utils.promisfyRemoveData(filePath, modifiedData);
+    await promisfyWriteFile(filePath, modifiedTodoString);
 
-    // const updatedTodoList=todos.filter((data)=>parseInt(data.todoId)!==parseInt(todoId))
-    // console.log(updatedTodoList)
-    // currentTodos = updatedTodoList.reduce((prevContent, currentObject) => {
-    //     prevContent += `${currentObject.todoId}|${currentObject.todoTask}\r\n`;
-    //     return prevContent;
-    //   }, '');
-    // console.log(currentTodos);
-    // await promisfyWriteFile(path,currentTodos)
-    // return "Updated"
-
-    // const data = await utils.promisfyReadFiles(filePath);
-    // data.splice((parseInt(id) - 1), 1);
-    // const modifiedData = data.map((element, index) => {
-    //     if (index >= (parseInt(id) - 1)) {
-    //         element = `${(index + 1).toString()}|${element.split('|')[1]}`;
-    //     }
-    //     return element;
-    // });
-    // await utils.promisfyRemoveData(filePath, modifiedData);
 }
 
 module.exports = {
@@ -80,28 +50,4 @@ module.exports = {
     postTodoService,
     updateTodoService,
     deleteTodoService
-}
-
-const updateTodo = async (todoId, newTodoTask) => {
-    const todos = await listTodos();
-    let filteredTodos = todos.filter((todo) => parseInt(todo.todoId) === parseInt(todoId));
-    const oldData = filteredTodos[0].todoTask
-    await updateFile(path, oldData, newTodoTask)
-    const todoList = listTodos();
-    return todoList;
-}
-
-const deleteTodoTask = async (todoId) => {
-    const todos = await listTodos();
-    console.log(todos)
-    console.log(todoId)
-    const updatedTodoList = todos.filter((data) => parseInt(data.todoId) !== parseInt(todoId))
-    console.log(updatedTodoList)
-    currentTodos = updatedTodoList.reduce((prevContent, currentObject) => {
-        prevContent += `${currentObject.todoId}|${currentObject.todoTask}\r\n`;
-        return prevContent;
-    }, '');
-    console.log(currentTodos);
-    await promisfyWriteFile(path, currentTodos)
-    return "Updated"
 }
